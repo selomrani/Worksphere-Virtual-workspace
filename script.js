4fb42e447187d7c79afec04bf022ac736189d81d
Soufyane <elomranisoufyan@gmail.com>
2025-11-18 19:16:42 +0100

    added functionality to add new staff and render cards

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let staff = []
 async function fetchjson(file) {
-        let response = await fetch(file)
-        let data = await response.json()
-        console.log(data)
+  let response = await fetch(file)
+  let data = await response.json()
+  console.log(data)
 }
 fetchjson("data.json")
 
@@ -10,11 +11,55 @@ function isValidEmail(email) {
   return regex.test(email);
 }
 // let email ="elomranisoufyan@gmail.com"
+const addform = document.getElementById("addform");
 
-const addform = document.forms
-const addBtnsave = document.getElementById("addBtnsave")
-addform.addEventListener("submit" ,function(e){
-  e.preventDefault()
-  let emailtest = getElementById("emailadd").value;
-  console.log(isValidEmail(emailtest))
-})
+addform.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const fname = document.getElementById("fname-add").value;
+  const lname = document.getElementById("lname-add").value;
+  const email = document.getElementById("emailadd").value;
+  const phone = document.getElementById("phone-add").value;
+  const img = document.getElementById("photo-add").value;
+  const occupation = document.getElementById("occupation-add").value;
+  if (isValidEmail(email)) {
+    const newstaff = {
+      fname: fname,
+      lname: lname,
+      email: email,
+      phone: phone,
+      img: img,
+      occupation: occupation,
+    };
+    staff.push(newstaff);
+    console.log(staff);
+    renderminicards()
+  } else {
+    alert("Plese enter valid infos");
+  }
+});
+const minicardsrender = document.getElementById("minicardsrender");
+function renderminicards(){
+  minicardsrender.innerHTML="";
+  staff.forEach(staff=>{
+    const cardyy = document.createElement("div");
+    cardyy.innerHTML=`<div class="card my-3">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <img src="https://intranet.youcode.ma/storage/users/profile/1558-1760996258.png" class="rounded-circle me-3"
+                                    alt="Profile Picture" style="width: 60px; height: 60px;">
+                                <div>
+                                    <h6 class="card-title mb-0">${staff.fname} ${staff.lname}</h6>
+                                    <p class="card-text text-muted mb-0">${staff.email}</p>
+                                </div>
+                                <div class="ms-auto d-flex flex-column gap-1">
+                                    <button type="button" class="btn btn-success btn-sm">Assign</button>
+                                    <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal"
+                                        data-bs-target="#modify" aria-label="Edit">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>`
+                        minicardsrender.appendChild(cardyy)
+  })
+};
