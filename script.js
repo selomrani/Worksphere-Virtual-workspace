c941d7b375eeab1246e58abee678c5ea06cb7cac
Soufyan <elomranisoufyan@gmail.com>
2025-11-20 14:02:54 +0100

    fixed add staff function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let staff = []
 const roomOccupationMap = {
-    'conferenceroom': 'manager',
-    'reception': 'receptionist',
-    'serversroom': 'IT guy',
-    'securityroom': 'security officer',
-    'staff': 'Cleaning staff',
-    'vault': 'security officer'
+  'conferenceroom': 'manager',
+  'reception': 'receptionist',
+  'serversroom': 'IT guy',
+  'securityroom': 'security officer',
+  'staff': 'Cleaning staff',
+  'vault': 'security officer'
 };
 
 async function fetchjson(file) {
@@ -21,9 +21,8 @@ function isValidEmail(email) {
   return regex.test(email);
 }
 
-
+function addnewstaff(){
 const addform = document.getElementById("addform");
-
 addform.addEventListener("submit", (event) => {
   event.preventDefault();
   const fname = document.getElementById("fname-add").value;
@@ -32,7 +31,6 @@ addform.addEventListener("submit", (event) => {
   const phone = document.getElementById("phone-add").value;
   const img = document.getElementById("photo-add").value;
   const occupation = document.getElementById("occupation-add").value;
-
   if (isValidEmail(email)) {
     const newstaff = {
       fname: fname,
@@ -48,8 +46,10 @@ addform.addEventListener("submit", (event) => {
   } else {
     alert("Plese enter valid infos");
   }
+  
 
-});
+})};
+addnewstaff()
 function storeStaffDataToLocalStorage() {
   const staffLocal = JSON.stringify(staff);
   localStorage.setItem("staffData", staffLocal);
@@ -95,18 +95,18 @@ function renderStaffListToModal(occupationType) {
   const stafflist = document.getElementById('stafflist');
   const modalTitle = document.getElementById('addtoroomLabel');
   if (modalTitle) {
-      modalTitle.textContent = `Available staff`;
+    modalTitle.textContent = `Available staff`;
   }
-  
+
   stafflist.innerHTML = '';
-  
+
   const unassignedStaff = staff.filter(staffMember => staffMember.occupation === occupationType);
-  
+
   if (unassignedStaff.length === 0) {
-      stafflist.innerHTML = `<p class="text-center text-muted">No unassigned ${occupationType} staff available.</p>`;
-      return;
+    stafflist.innerHTML = `<p class="text-center text-muted">No unassigned ${occupationType} staff available.</p>`;
+    return;
   }
-  unassignedStaff.forEach(staffMember => { 
+  unassignedStaff.forEach(staffMember => {
     const profile = document.createElement('div');
     profile.innerHTML = `
       <div class="card my-3">
@@ -133,15 +133,16 @@ const addStaffButtons = document.querySelectorAll(".add-staff-btn");
 
 addStaffButtons.forEach(button => {
   button.addEventListener('click', function () {
-    const roomId = this.getAttribute('data-room-id'); 
+    const roomId = this.getAttribute('data-room-id');
     const requiredOccupation = roomOccupationMap[roomId];
-    
     if (requiredOccupation) {
-        renderStaffListToModal(requiredOccupation);
+      renderStaffListToModal(requiredOccupation);
     } else {
-        console.error('No occupation mapped for room ID:', roomId);
+      console.log("No room with such id was found")
     }
   });
 });
-
 getDataFromLocalStorage()
+
+
+const deletebtns = document.getElementById("")
